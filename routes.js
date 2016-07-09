fdb1cde9978182f5f24a96a8c65e236998c9eeee
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2016-07-09 22:47:12 +0530

    List of facts

--- routes.js
+++ routes.js
@@ -146,7 +146,6 @@ router.route('/users/:userId')
 			user.publishedFacts = req.body.publishedFacts;
 			if(req.body.favFacts){
 				console.log(req.body.favFacts + " " + typeof req.body.favFacts)
-				user.favFacts = req.body.favFacts.split(",")
 				user.favFacts.forEach(function(item){
 					console.log(item);
 				})
